40a81339c678eeeb27f7b004506eba2d550a7d63
Bruno Moyano <108307019+BEMP1@users.noreply.github.com>
2022-10-04 08:30:34 -0300

    Add files via upload

---
+++ consigna2tp9Ayd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	struct Chofer {
+		string nombreApellido;
+		int dni;
+		int edad;
+	};
+	struct Salida {
+		int hora;
+		int minuto;
+	};
+	struct Llegada {
+		int hora;
+		int minuto;
+	};
+	struct Omnibus {
+		int nroUnidad;
+		string origen;
+		string destino;
+		Salida salida;
+		Llegada llegada;
+		Chofer chofer;
+		char estadoAsientos[19];
+	};
+	return 0;
+}
+
